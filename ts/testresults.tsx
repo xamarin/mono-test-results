20b3f542888100f926bcd75d6b7ddfbbab426881
Andi McClure <andi.mcclure@xamarin.com>
2016-07-14 16:38:06 -0400

    Fetch babysitter

--- ts/testresults.tsx
+++ ts/testresults.tsx
@@ -1,10 +1,24 @@
 /// <reference path="../typings/tsd.d.ts" />
 /// <reference path="./helper.ts" />
 
+const max_build_queries = 10
+
 // Lanes list
 
-function jenkinsUrl(lane:string) {
-	return "https://jenkins.mono-project.com/job/"+lane+"/api/json"
+function jenkinsBaseUrl(lane:string) {
+	return "https://jenkins.mono-project.com/job/" + lane
+}
+
+function jenkinsLaneUrl(lane:string) {
+	return jenkinsBaseUrl(lane) + "/api/json"
+}
+
+function jenkinsBuildUrl(lane:string, id:string) {
+	return jenkinsBaseUrl(lane) + "/" + id
+}
+
+function jenkinsBabysitterUrl(lane:string, id:string) {
+	return jenkinsBuildUrl(lane, id) + "/artifact/babysitter_report.json_lines"
 }
 
 let jenkinsLaneSpecs = [ // Name, Regular Jenkins job, PR Jenkins job
@@ -16,29 +30,63 @@ let jenkinsLaneSpecs = [ // Name, Regular Jenkins job, PR Jenkins job
 	["Windows 32-bit", "z/label=w32",                           "w"]
 ]
 
+class Build {
+	id: string
+	loaded: boolean
+
+	constructor(id: string) {
+		this.id = id
+		this.loaded = false
+	}
+}
+
 // Object tracks/downloads one lane's worth of tests
 // TODO:
 // - Fetch jobs list from Lane url
 // - Fetch URL like https://jenkins.mono-project.com/job/test-mono-mainline/label=debian-amd64/3063/artifact/babysitter_report.json_lines
 //   from job number
 class Lane {
-	name: string
+	name: string  // Human-readable
+	tag: string   // URL component
 	url: string
 	loaded: boolean
 	failed: boolean
+	builds: Build[]
 
 	constructor(name:string, laneName:string) {
 		this.name = name
-		this.url = jenkinsUrl(laneName)
+		this.tag = laneName
+		this.url = jenkinsLaneUrl(laneName)
 		this.loaded = false
 		this.failed = false
+		this.builds = []
 	}
 
 	load() {
 		if ('debug' in options) console.log("lane loading url", this.url)
-		$.get(this.url, result => {
+		$.get(this.url, laneResult => {
 			this.loaded = true
-			if ('debug' in options) console.log("lane loaded url", this.url, "result:", result)
+			if ('debug' in options) console.log("lane loaded url", this.url, "result:", laneResult)
+			let queries = 0
+
+			for (let buildInfo of laneResult.builds) {
+				let build = new Build(buildInfo.number)
+				this.builds.push(build)
+
+				let babysitterUrl = jenkinsBabysitterUrl(this.tag, build.id)
+				if ('debug' in options) console.log("build", build.id, "for lane", this.name, "loading url", babysitterUrl)
+
+				$.get(babysitterUrl, buildResult => {
+					if ('debug' in options) console.log("build loaded url", babysitterUrl, "result:", buildResult)
+
+				}).fail(() => {
+					console.log("Failed to load url for lane", babysitterUrl);
+				})
+
+				queries++
+				if (queries >= max_build_queries)
+					break
+			}
 
 			invalidateUi()
 		}).fail(() => {
@@ -53,7 +101,7 @@ class Lane {
 let lanes: Lane[] = []
 
 for (let spec of jenkinsLaneSpecs) {
-	// Spec is a triplet of name, normal URL, PR URL
+	// Spec is a triplet of name, normal URL tag, PR URL tag
 	for (let d = 0; d < 2; d++) {
 		let name = spec[0]
 		if (d)
